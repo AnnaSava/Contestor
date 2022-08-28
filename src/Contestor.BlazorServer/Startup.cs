57e9b6f4578efb9960a7d36bfe5e643300349668
Anna Savina <anna.s.savina@gmail.com>
2022-08-28 20:56:10 +0300

    SignalR Chat from MS docs

--- src/Contestor.BlazorServer/Startup.cs
+++ src/Contestor.BlazorServer/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Contestor.BlazorServer.Hubs;
+using System.Linq;
 
 namespace Contestor.BlazorServer
 {
@@ -30,11 +33,18 @@ namespace Contestor.BlazorServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +66,7 @@ namespace Contestor.BlazorServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
+                endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
